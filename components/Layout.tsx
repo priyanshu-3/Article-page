5aff50263de1eedd24e596f3e66b18516f4c2c45
priyanshu-3 <137265392+priyanshu-3@users.noreply.github.com>
2026-08-22 12:54:43 +0530

    refactor: re-export unified Layout from src/components/Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,2 @@
-import { ReactNode } from 'react';
-import Navbar from './Navbar';
-
-interface LayoutProps {
-  children: ReactNode;
-}
-
-export default function Layout({ children }: LayoutProps) {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50 text-gray-900 font-sans antialiased">
-      <Navbar />
-      <main className="flex-1 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        {children}
-      </main>
-    </div>
-  );
-}
+import Layout from '../src/components/Layout';
+export default Layout;
